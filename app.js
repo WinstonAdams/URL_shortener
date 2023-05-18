3da275edef278a408443754c4bc87c2e1c93a38a
WinstonAdams <apollo07291@gmail.com>
2023-05-18 22:49:03 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+//* 外部套件
 const express = require('express')
+const exphbs = require('express-handlebars')
 
+//* 自己設定的檔案
 require('./config/mongoose')
 
 const app = express()
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
 app.get('/', (req, res) => {
-  res.send('hello')
+  res.render('index')
 })
 
 app.listen(3000, () => {
